8935525382f81a246d2905573bff7021d51f7f64
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-09-03 15:28:37 +0300

    First commit

--- oop-script.js
+++ oop-script.js
@@ -3,17 +3,17 @@
 class App {
     static async run() {
         const movies = await APIService.fetchMovies()
-        HomePage.renderMovies(movies);
+        HomePage.renderMovies(movies)
     }
 }
 
 class APIService {
-    static TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+    static TMDB_BASE_URL = 'https://api.themoviedb.org/3'
     static async fetchMovies() {
         const url = APIService._constructUrl(`movie/now_playing`)
         const response = await fetch(url)
         const data = await response.json()
-        return data.results.map(movie => new Movie(movie))
+        return data.results.map((movie) => new Movie(movie))
     }
     static async fetchMovie(movieId) {
         const url = APIService._constructUrl(`movie/${movieId}`)
@@ -22,44 +22,44 @@ class APIService {
         return new Movie(data)
     }
     static _constructUrl(path) {
-        return `${this.TMDB_BASE_URL}/${path}?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}`;
+        return `${this.TMDB_BASE_URL}/${path}?api_key=${atob(
+      'bae5a03c227c33b8d9842f4e6c132889='
+    )}`
     }
 }
 
 class HomePage {
-    static container = document.getElementById('container');
+    static container = document.getElementById('container')
     static renderMovies(movies) {
-        movies.forEach(movie => {
-            const movieDiv = document.createElement("div");
-            const movieImage = document.createElement("img");
-            movieImage.src = `${movie.backdropUrl}`;
-            const movieTitle = document.createElement("h3");
-            movieTitle.textContent = `${movie.title}`;
-            movieImage.addEventListener("click", function() {
-                Movies.run(movie);
-            });
+        movies.forEach((movie) => {
+            const movieDiv = document.createElement('div')
+            const movieImage = document.createElement('img')
+            movieImage.src = `${movie.backdropUrl}`
+            const movieTitle = document.createElement('h3')
+            movieTitle.textContent = `${movie.title}`
+            movieImage.addEventListener('click', function() {
+                Movies.run(movie)
+            })
 
-            movieDiv.appendChild(movieTitle);
-            movieDiv.appendChild(movieImage);
-            this.container.appendChild(movieDiv);
+            movieDiv.appendChild(movieTitle)
+            movieDiv.appendChild(movieImage)
+            this.container.appendChild(movieDiv)
         })
     }
 }
 
-
 class Movies {
     static async run(movie) {
         const movieData = await APIService.fetchMovie(movie.id)
-        MoviePage.renderMovieSection(movieData);
+        MoviePage.renderMovieSection(movieData)
         APIService.fetchActors(movieData)
-
     }
 }
 
 class MoviePage {
-    static container = document.getElementById('container');
+    static container = document.getElementById('container')
     static renderMovieSection(movie) {
-        MovieSection.renderMovie(movie);
+        MovieSection.renderMovie(movie)
     }
 }
 
@@ -80,24 +80,24 @@ class MovieSection {
         </div>
       </div>
       <h3>Actors:</h3>
-    `;
+    `
     }
 }
 
 class Movie {
-    static BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780';
+    static BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'
     constructor(json) {
-        this.id = json.id;
-        this.title = json.title;
-        this.releaseDate = json.release_date;
-        this.runtime = json.runtime + " minutes";
-        this.overview = json.overview;
-        this.backdropPath = json.backdrop_path;
+        this.id = json.id
+        this.title = json.title
+        this.releaseDate = json.release_date
+        this.runtime = json.runtime + ' minutes'
+        this.overview = json.overview
+        this.backdropPath = json.backdrop_path
     }
 
     get backdropUrl() {
-        return this.backdropPath ? Movie.BACKDROP_BASE_URL + this.backdropPath : "";
+        return this.backdropPath ? Movie.BACKDROP_BASE_URL + this.backdropPath : ''
     }
 }
 
-document.addEventListener("DOMContentLoaded", App.run);
+document.addEventListener('DOMContentLoaded', App.run)
