cf19b1c61067668abda8a2435d3b326eaa3a61e8
mhakmi <marwahakmi@gmail.com>
2022-09-05 19:25:15 +0300

    actors js part

--- oop-script.js
+++ oop-script.js
@@ -4,6 +4,8 @@ class App {
     static async run() {
         const movies = await APIService.fetchMovies()
         HomePage.renderMovies(movies);
+        const actors = await APIService.fetchActors()
+        HomePage.renderActors(actors);
     }
 }
 
@@ -19,7 +21,21 @@ class APIService {
         const url = APIService._constructUrl(`movie/${movieId}`)
         const response = await fetch(url)
         const data = await response.json()
+        console.log('movie', data)
         return new Movie(data)
+    }
+    static async fetchActors() {
+        const url = APIService._constructUrl(`person/popular`)
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log("Actors", data)
+        return data.results.map(actor => new Actor(actor))
+    }
+    static async fetchActor(actorId) {
+        const url = APIService._constructUrl(`person/${actorId}`)
+        const response = await fetch(url)
+        const data = await response.json()
+        return new Actor(data)
     }
     static _constructUrl(path) {
         return `${this.TMDB_BASE_URL}/${path}?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}`;
@@ -42,6 +58,22 @@ class HomePage {
             movieDiv.appendChild(movieTitle);
             movieDiv.appendChild(movieImage);
             this.container.appendChild(movieDiv);
+        })
+    }
+    static renderActors(actors) {
+        actors.forEach(actor => {
+            const actorDiv = document.createElement("div");
+            const actorImage = document.createElement("img");
+            actorImage.src = `${actor.profileUrl}`;
+            const actorName = document.createElement("h3");
+            actorName.textContent = `${actor.name}`;
+            actorImage.addEventListener("click", function () {
+                Actors.run(actor);
+            });
+
+            actorDiv.appendChild(actorName);
+            actorDiv.appendChild(actorImage);
+            this.container.appendChild(actorDiv);
         })
     }
 }
@@ -99,5 +131,52 @@ class Movie {
         return this.backdropPath ? Movie.BACKDROP_BASE_URL + this.backdropPath : "";
     }
 }
+
+class ActorPage {
+    static container = document.getElementById('container');
+    static renderActorSection(actor) {
+        ActorSection.renderActor(actor);
+    }
+}
+
+class ActorSection {
+    static renderActor(actor) {
+        ActorPage.container.innerHTML = `
+      <div class="row">
+        <div class="col-md-4">
+          <img id="actor-profile" src=${actor.profileUrl}> 
+        </div>
+        <div class="col-md-8">
+          <h2 id="actor-name">${actor.name}</h2>
+          <p id="actor-birthday">${actor.birthday}</p>
+          <p id="actor-birthplace">${actor.birthplace}</p>
+          <p id="actor-biography">${actor.biography}</p>
+        </div>
+      </div>
+    `;
+    }
+}
+
+class Actors {
+    static async run(actor) {
+        const actorData = await APIService.fetchActor(actor.id)
+        ActorPage.renderActorSection(actorData);
+        // APIService.fetchActors(actorData)
+        // APIService.fetchMovies(actorData)
+    }
+}
+
+class Actor {
+    static PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185';
+    constructor(json) {
+        this.id = json.id;
+        this.name = json.name;
+        this.profilePath = json.profile_path;
+    }
+    get profileUrl() {
+        return this.profilePath ? Actor.PROFILE_BASE_URL + this.profilePath : "";
+    }
+}
+
 
 document.addEventListener("DOMContentLoaded", App.run);
